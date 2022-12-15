a8f6e405c982c87ab9268d77af9f0692e4c48a6a
oscar <camamoblamientos@gmail.com>
2022-12-15 08:58:18 -0300

    agregue el problema 6

--- problema6/codigo.js
+++ problema6/codigo.js
@@ -1,19 +1,31 @@
-let input = document.getElementById('input');
-let buscar = document.getElementById('buscar');
-let nombre = "";
-
-let datos={
-    tipo:"",
-}
-//nombre, numero tipo peso altura imagen
-buscar.addEventListener('click', ()=>{
-    fetch(`https://pokeapi.co/api/v2/pokemon/${nombre}`)
-    .then((response)=>response.json())
-    .then((data)=> console.log(data))
-})
-
+const input = document.getElementById('input')
 
 input.addEventListener('change', (e)=>{
-    nombre = e.target.value
-})
+    idPokemon = e.target.value
+    
 
+
+const getPokemon = (idPokemon) => {
+    let data =  fetch(`https://pokeapi.co/api/v2/pokemon/${idPokemon}`)
+        .then((response) => response.json())
+        .then((data) => crearPokemon(data))
+}
+
+getPokemon(idPokemon)
+
+//Nombre, número, tipo, peso, altura y una imágen
+const crearPokemon = (pokemon)=>{
+    console.log(pokemon)
+    console.log(pokemon.types[0].type.name)
+    const card = document.getElementById('card')
+    const parrafo = document.createElement('p')
+    card.innerHTML = `<img src=${pokemon.sprites.other.dream_world.front_default} alt="pokemon">
+    <h3>${pokemon.name}</h3>
+    <p>numero: ${pokemon.id}</p> 
+    <p>Altura: ${pokemon.height}</p>
+    <p>tipo :${pokemon.types[0].type.name}</p>
+    <p>peso :${pokemon.weight}</p> `
+
+}
+
+})
